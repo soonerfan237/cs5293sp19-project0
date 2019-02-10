d676eaace26157d3fc0f2df56d46800195a68573
soonerfan237 <soonerfan237@gmail.com>
2019-02-10 16:57:30 -0600

    Initial project creation.

---
+++ project0/main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Example main.py
+import argparse
+
+import project0
+
+def main(url):
+    # Download data
+    project0.fetchincidents(url)
+
+    # Extract Data
+    incidents = project0.extractincidents()
+	
+    # Create Dataase
+    db = project0.createdb()
+	
+    # Insert Data
+    project0.populatedb(db, incidents)
+	
+    # Print Status
+    project0.status(db)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--arrests", type=str, required=True, 
+                         help="The arrest summary url.")
+     
+    args = parser.parse_args()
+    if args.arrests:
+        main(args.arrests)
