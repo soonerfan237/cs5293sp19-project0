deb88a11ee3d40a8a0d6359e58da65a9383b65c6
soonerfan237 <soonerfan237@gmail.com>
2019-02-13 12:57:12 -0600

    Working on test_fields test case. Making sure proper number of incidents are parsed from a test file.

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -10,12 +10,9 @@ def test_extract_request():
         test_str = testfile.read()
     incidents = project0.parseincidents(test_str)
     for incident in incidents:
-        print("----------------INCIDENT-----------------")
-        for field in incident:
-            print("FIELD: " + field)
-    print(" ")
-    print("NUMBER OF INCIDENTS: " + str(len(incidents)))
+        for field in incidents:
+            print(field)
     db = project0.createdb()
     project0.populatedb(db, incidents)
-    assert 1 == 2
+    assert len(incidents) == 14
 
