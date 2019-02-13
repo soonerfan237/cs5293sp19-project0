c51e95123564b4a2b5b62929185c9a70dab62635
soonerfan237 <soonerfan237@gmail.com>
2019-02-12 19:47:49 -0600

    Installed pytest.  Set up dummy test scripts.

---
+++ tests/test_download.py
@@ -0,0 +1,12 @@
+import pytest
+
+import project0
+from project0 import main
+from project0 import project0
+
+def test_download_sanity():
+    assert 1 == 1
+
+
+#def test_download_size():
+    #assert len(main.download()) == 135131
---
+++ tests/test_fields.py
@@ -0,0 +1,10 @@
+import pytest
+
+import project0
+from project0 import main
+from project0 import project0
+
+def test_extract_request():
+    #text = main.download()
+    #promises = main.extract_requests(text)
+    assert 1 == 1
